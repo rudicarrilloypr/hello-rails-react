fa24ccfa0fa55445d4f165af081a2dc3e915ea16
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-10-30 13:24:16 -0600

    Fix issues

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Greeting from './Greeting';
-import { GreetingProvider } from '../contexts/GreetingContext';
+import { Provider } from 'react-redux';
+import store from '../store/store';
 
 function App() {
   return (
-    <GreetingProvider>
+    <Provider store={store}>
       <Router>
         <Routes>
           <Route path="/greeting" element={<Greeting />} />
         </Routes>
       </Router>
-    </GreetingProvider>
+    </Provider>
   );
 }
 
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,19 +1,24 @@
-import React, { useContext, useEffect } from 'react';
-import { GreetingContext } from '../contexts/GreetingContext';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setGreeting } from '../actions/greetingActions';
 
 function Greeting() {
-  const { state, dispatch } = useContext(GreetingContext);
+  const dispatch = useDispatch();
+
+  // Usamos useSelector para obtener el saludo desde el store de Redux
+  const greetingMessage = useSelector(state => state.greeting);
 
   useEffect(() => {
     // Fetch the greeting from the Rails API
     fetch('/random_greeting')
       .then((response) => response.json())
       .then((data) => {
-        dispatch({ type: 'SET_GREETING', payload: data.message });
+        // Usa el action setGreeting para enviar el saludo al store de Redux
+        dispatch(setGreeting(data.message));
       });
   }, [dispatch]);
 
-  return <h2>{state.message}</h2>;
+  return <h2>{greetingMessage}</h2>;
 }
 
 export default Greeting;
